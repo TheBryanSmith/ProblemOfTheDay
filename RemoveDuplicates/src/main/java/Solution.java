102fc424aec699811daeefa05f794909fffa9eca
THEBRYANSMITH <bryanstechinfo@gmail.com>
2025-01-10 13:39:15 -0500

    Finished

--- RemoveDuplicates/src/main/java/Solution.java
+++ RemoveDuplicates/src/main/java/Solution.java
@@ -1,2 +1,15 @@
 public class Solution {
+//int[] nums = [1,1,2,3,4,5,5,6];
+    // return int[] expected [1,2,3,4,5,6];
+
+    int DuplicateDeleter(int[] nums){
+        int uniqueIndex = 1;
+      for(int x = 1; x < nums.length; x++){
+          if(nums[x] != nums[x-1]){
+             nums[uniqueIndex] = nums[x];
+             uniqueIndex++;
+          }
+      }
+      return uniqueIndex;
+    }
 }
