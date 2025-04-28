88f47e3e8fa11fd891e1186fe6d97445da7b2983
THEBRYANSMITH <bryanstechinfo@gmail.com>
2025-04-28 11:50:39 -0400

    Problem to get index

---
+++ RemoveDuplicates/src/main/java/GetIndex.java
@@ -0,0 +1,18 @@
+public class GetIndex {
+
+    /**
+
+     Find the position of a number in an array.
+     For example, the position of 7 in {0,1,7,3,4} is 2.*
+     @param arr an int array.
+     @param n a possible value of arr.
+     @return the first index (position) of n in arr. if n does not exist in arr, return -1.*/
+    public int getIndex(int[] arr, int n){
+    for(int x  = 0; x < arr.length; x++){
+        if(arr[x] == n){
+            return x;
+        }
+    }
+        return -1;
+    }
+}
